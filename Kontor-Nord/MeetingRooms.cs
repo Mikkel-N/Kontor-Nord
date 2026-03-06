5afd5a996f3134c25579b1aac2ba2347c75ba6fa
AndOr <skou.andrew@gmail.com>
2026-03-06 11:20:27 +0100

    Fjerner original optegnelse til meeting rooms og tilføjer enum 'RoomName'

--- Kontor-Nord/MeetingRooms.cs
+++ Kontor-Nord/MeetingRooms.cs
@@ -2,14 +2,11 @@
 
 public class MeetingRooms
 {
-	private int _numberOfRooms;
-	private string _roomNames;
-	private bool _isRoomAvailable;
 
-
-
-	public MeetingRooms(int _numberOfRooms, int _roomNames, bool _isRoomAvailable)
+	public enum RoomName
 	{
-
+		Alfa,
+		Beta,
+		Gamma
 	}
 }
